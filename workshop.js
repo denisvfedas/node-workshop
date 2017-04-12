a364d15dd19fb5c91b1cd9065775e3b0ea1f05b1
Denys <denisvfedas@gmail.com>
2017-04-12 20:21:03 +0000

    getCurrentTemperatureAtPosition

--- workshop.js
+++ workshop.js
@@ -33,8 +33,19 @@ function getAddressPosition(address) {
     });
 }
 
-function getCurrentTemperatureAtPosition(position) {
+//   1. Go to [Dark Sky API](https://darksky.net/dev/) and read the documentation
+//   2. Signup and get a free API key
+//   3. Complete the code of the function. The `position` parameter is an object with `lat` and `lng`.
+//   4. Make sure your function only returns a `Promise` for the current temperature (a number) and nothing else
+  
 
+function getCurrentTemperatureAtPosition(position) {
+    return request('https://api.darksky.net/forecast/0046ffbcf26d5b029c9499baa0c950b9/' +  position.lat + ',' + position.lng)
+    .then(function(responce) {
+        var data = JSON.parse(responce);
+        var temp = data.currently.temperature;
+        return temp;
+    });
 }
 
 function getCurrentTemperature(address) {
